069fdf176453c7d46e0f6478001c0d0e9f40708c
jiaLEE <jia9510@naver.com>
2023-01-16 19:05:50 +0900

    @Transactional 추가.

--- src/main/java/dev/practice/order/domain/partner/PartnerServiceImpl.java
+++ src/main/java/dev/practice/order/domain/partner/PartnerServiceImpl.java
@@ -1,9 +1,11 @@
 package dev.practice.order.domain.partner;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.beans.Transient;
 import java.lang.annotation.Retention;
 
 @Slf4j
@@ -15,6 +17,7 @@ public class PartnerServiceImpl implements PartnerService{
     private final PartnerReader partnerReader;
 
     @Override
+    @Transactional
     public PartnerInfo registerPartner(PartnerCommand command) {
         // 개인적으로 service 안에 로직은 3줄로 정의가능해야한다. 그 외의 자세한 구현은 infraStructure로 넘겨야한다.
         // 1. comman로 들어온 정보를 -> initPartner
@@ -27,6 +30,7 @@ public class PartnerServiceImpl implements PartnerService{
     }
 
     @Override
+    @Transactional
     public PartnerInfo getPartnerInfo(String partnerToken) {
         // 1. partnerToken으로 partner를 읽어온다.
         // 2. partner -> partnerInfo로 리턴한다.
@@ -35,6 +39,7 @@ public class PartnerServiceImpl implements PartnerService{
     }
 
     @Override
+    @Transactional
     public PartnerInfo enablePartner(String partnerToken) {
         // 1. partnerToken으로 partner를 읽어온다.
         // 2. partner.enable
@@ -44,6 +49,7 @@ public class PartnerServiceImpl implements PartnerService{
     }
 
     @Override
+    @Transactional
     public PartnerInfo disablePartner(String partnerToken) {
         // 1. partnerToken으로 partner를 읽어온다.
         // 2. partner.enable
